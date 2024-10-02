638b7c55e93c2710a8ad689ea70f6b01f5e5ed46
parkjaehak <atom8426@gmail.com>
2024-10-02 11:16:02 +0900

    Initial setting

---
+++ src/main/java/org/userservice/userservice/HealthCheckController.java
@@ -0,0 +1,15 @@
+package org.userservice.userservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+    @GetMapping
+    public String healthcheck() {
+        return "Hello world";
+    }
+
+}
