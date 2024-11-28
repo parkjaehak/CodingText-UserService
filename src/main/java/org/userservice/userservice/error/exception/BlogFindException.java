2741f793f1cf976b50f0fbbc0472a66123dd53d2
parkjaehak <atom8426@gmail.com>
2024-11-28 19:44:09 +0900

    modify: 블로그 조회시 에러코드 수정

--- src/main/java/org/userservice/userservice/error/exception/BlogFindException.java
+++ src/main/java/org/userservice/userservice/error/exception/BlogFindException.java
@@ -4,6 +4,6 @@ import org.userservice.userservice.error.ErrorCode;
 
 public class BlogFindException extends BusinessException{
     public BlogFindException(String message) {
-        super(message, ErrorCode.BLOG_DELETION_FAILED);
+        super(message, ErrorCode.BLOG_FIND_FAILED);
     }
 }
